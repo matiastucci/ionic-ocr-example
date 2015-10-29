87ca52db90480cad1e4de3f3e71229075ef7dcbf
Matias <matiastucci@gmail.com>
2015-10-29 10:53:52 -0300

    use ocrad.js

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+angular.module('starter', ['ionic','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -17,3 +17,77 @@ angular.module('starter', ['ionic'])
     }
   });
 })
+
+.controller('CaptureCtrl', function($scope, $ionicActionSheet, $ionicLoading, $ionicPlatform, $cordovaCamera) {
+
+  $ionicPlatform.ready(function() {
+
+    $scope.showAnalyzeButton = false;
+
+    var self = this;
+
+    this.showLoading = function() {
+      $ionicLoading.show({
+        template: '<ion-spinner></ion-spinner>'
+      });
+    };
+
+    this.hideLoading = function(){
+      $ionicLoading.hide();
+    };
+
+    this.getPicture = function(index){
+
+      var sourceType = index === 0 ? Camera.PictureSourceType.PHOTOLIBRARY : Camera.PictureSourceType.CAMERA;
+      var options = {
+        quality: 100,
+        destinationType: Camera.DestinationType.DATA_URL,
+        sourceType: sourceType,
+        allowEdit: true,
+        encodingType: Camera.EncodingType.JPEG,
+        popoverOptions: CameraPopoverOptions,
+        saveToPhotoAlbum: false,
+        correctOrientation:true
+      };
+
+      $cordovaCamera.getPicture(options).then(function(imageData) {
+        var image = document.getElementById('pic');
+        image.src = "data:image/jpeg;base64," + imageData;
+        $scope.showAnalyzeButton = true;
+      }, function(err) {
+          console.log(err);
+      });
+
+    };
+    
+  });
+
+  $scope.showActionSheet = function(){
+    var hideSheet = $ionicActionSheet.show({
+      buttons: [
+       { text: 'Choose Photo' },
+       { text: 'Take Photo' }
+      ],
+      cancelText: 'Cancel',
+      cancel: function() {
+        console.log('cancel');
+      },
+      buttonClicked: function(index) {
+        getPicture(index);
+       return true;
+      }
+    });
+  };
+
+  $scope.showActionSheet();
+
+  $scope.testOcrad = function(){
+    self.showLoading();
+    OCRAD(document.getElementById("pic"), function(text){
+      self.hideLoading();
+      console.log(text);
+      alert(text);
+    });
+  } ; 
+
+});
