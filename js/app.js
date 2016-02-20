440ad7af3b44944dfdba0efe69b39d7db8ec4c5b
assafhp <orenor10@barak.net.il>
2016-02-20 15:14:08 +0200

    added camera browser fallback

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ angular.module('starter', ['ionic','ngCordova'])
   });
 })
 
-.controller('CaptureCtrl', function($scope, $ionicActionSheet, $ionicLoading, $ionicPlatform, $cordovaCamera) {
+.controller('CaptureCtrl', function($scope ,CameraSrv ,$ionicActionSheet, $ionicLoading, $ionicPlatform, $cordovaCamera) {
 
   $ionicPlatform.ready(function() {
 
@@ -38,29 +38,19 @@ angular.module('starter', ['ionic','ngCordova'])
 
     this.getPicture = function(index){
 
-      var sourceType = index === 0 ? Camera.PictureSourceType.PHOTOLIBRARY : Camera.PictureSourceType.CAMERA;
-      var options = {
-        quality: 100,
-        destinationType: Camera.DestinationType.DATA_URL,
-        sourceType: sourceType,
-        allowEdit: true,
-        encodingType: Camera.EncodingType.JPEG,
-        popoverOptions: CameraPopoverOptions,
-        saveToPhotoAlbum: false,
-        correctOrientation:true
-      };
-
-      $cordovaCamera.getPicture(options).then(function(imageData) {
-        var image = document.getElementById('pic');
-        image.src = "data:image/jpeg;base64," + imageData;
-        $scope.showAnalyzeButton = true;
-      }, function(err) {
+      CameraSrv.getPicture()
+        .then(function(imageData) {
+          var image = document.getElementById('pic');
+          image.src = "data:image/jpeg;base64," + imageData;
+          $scope.showAnalyzeButton = true;
+        })
+        .catch(function(err) {
           console.log(err);
-      });
-
+        });
     };
-    
   });
+
+  
 
   $scope.showActionSheet = function(){
     var hideSheet = $ionicActionSheet.show({
@@ -90,4 +80,76 @@ angular.module('starter', ['ionic','ngCordova'])
     });
   } ; 
 
-});
+})
+
+
+.factory('CameraSrv', ['$rootScope', '$q', '$window',function($rootScope, $q, $window) {
+
+  return {
+    getPicture: function(options) {
+
+      var deferred = $q.defer();
+
+      if (!$window.Camera) {
+
+        // create file input without appending to DOM
+        var fileInput = document.createElement('input');
+        fileInput.setAttribute('type', 'file');
+
+        fileInput.onchange = function() {
+          var file = fileInput.files[0];
+          var reader = new FileReader();
+          reader.readAsDataURL(file);
+          reader.onloadend = function () {
+            $rootScope.$apply(function() {
+              // strip beginning from string
+              var encodedData = reader.result.replace(/data:image\/jpeg;base64,/, '');
+              deferred.resolve(encodedData);
+            });
+          };
+        };
+
+        fileInput.click();
+      } 
+      else {
+        var sourceType = index === 0 ? Camera.PictureSourceType.PHOTOLIBRARY : Camera.PictureSourceType.CAMERA;
+        
+        // set some default options
+        var defaultOptions = {
+          quality: 100,
+          destinationType: Camera.DestinationType.DATA_URL,
+          sourceType: sourceType,
+          allowEdit: true,
+          encodingType: Camera.EncodingType.JPEG,
+          popoverOptions: CameraPopoverOptions,
+          saveToPhotoAlbum: false,
+          correctOrientation:true
+        };
+        
+        // allow overriding the default options
+        options = angular.extend(defaultOptions, options);
+
+        // success callback
+        var success = function(imageData) {
+          $rootScope.$apply(function() {
+            deferred.resolve(imageData);
+          });
+        };
+
+        // fail callback
+        var fail = function(message) {
+          $rootScope.$apply(function() {
+            deferred.reject(message);
+          });
+        };
+
+        // open camera via cordova
+        navigator.camera.getPicture(success, fail, options);
+      }
+
+      // return a promise
+      return deferred.promise;
+    }
+  };
+
+}]);
