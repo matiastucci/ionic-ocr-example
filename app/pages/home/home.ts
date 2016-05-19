0265cb3cdbca8c233234105aeca361e9405c5b19
Matias Tucci <matiastucci@gmail.com>
2016-05-19 06:09:15 +0200

    add camera button

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,36 @@
-import {Page} from 'ionic-angular';
+import {Platform, Page, NavController, ActionSheet} from 'ionic-angular';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor() {
+  constructor(private platform: Platform, private nav: NavController) { }
 
+  presentActionSheet() {
+    let actionSheet = ActionSheet.create({
+      buttons: [
+        {
+          text: 'Choose Photo',
+          handler: () => {
+            console.log('Choose Photo');
+          }
+        },
+        {
+          text: 'Take Photo',
+          handler: () => {
+            console.log('Take Photo');
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Cancel clicked');
+          }
+        }
+      ]
+    });
+    this.nav.present(actionSheet);
   }
+
 }
