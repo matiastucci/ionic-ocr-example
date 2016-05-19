89baac499fd3895b4e02df252a041a60102ba898
Matias Tucci <matiastucci@gmail.com>
2016-05-19 05:22:40 +0200

    init

---
+++ app/app.js
@@ -0,0 +1,24 @@
+import {App, Platform} from 'ionic-angular';
+import {StatusBar} from 'ionic-native';
+import {HomePage} from './pages/home/home';
+
+
+@App({
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  config: {} // http://ionicframework.com/docs/v2/api/config/Config/
+})
+export class MyApp {
+  static get parameters() {
+    return [[Platform]];
+  }
+
+  constructor(platform) {
+    this.rootPage = HomePage;
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+    });
+  }
+}
---
+++ app/pages/home/home.js
@@ -0,0 +1,11 @@
+import {Page} from 'ionic-angular';
+
+
+@Page({
+  templateUrl: 'build/pages/home/home.html'
+})
+export class HomePage {
+  constructor() {
+
+  }
+}
