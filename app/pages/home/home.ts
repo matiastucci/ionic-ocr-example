114e16c8691ff285faea1441ec3224780fdc1061
Matias Tucci <matiastucci@gmail.com>
2016-05-19 14:54:31 +0200

    analyze and restart

--- app/pages/home/home.ts
+++ app/pages/home/home.ts
@@ -1,10 +1,14 @@
-import {Platform, Page, NavController, ActionSheet} from 'ionic-angular';
+import {Page, Loading, NavController, ActionSheet} from 'ionic-angular';
+import {Camera} from 'ionic-native';
 
 @Page({
   templateUrl: 'build/pages/home/home.html'
 })
 export class HomePage {
-  constructor(private platform: Platform, private nav: NavController) { }
+
+  srcImage: string;
+
+  constructor(private nav: NavController) { }
 
   presentActionSheet() {
     let actionSheet = ActionSheet.create({
@@ -12,25 +16,62 @@ export class HomePage {
         {
           text: 'Choose Photo',
           handler: () => {
-            console.log('Choose Photo');
+            this.getPicture(0); // 1 == Library
           }
         },
         {
           text: 'Take Photo',
           handler: () => {
-            console.log('Take Photo');
+            this.getPicture(1); // 1 == Camera
+          }
+        },
+        {
+          text: 'Demo Photo',
+          handler: () => {
+            this.srcImage = 'demo.png';
           }
         },
         {
           text: 'Cancel',
-          role: 'cancel',
-          handler: () => {
-            console.log('Cancel clicked');
-          }
+          role: 'cancel'
         }
       ]
     });
     this.nav.present(actionSheet);
+  }
+
+  getPicture (sourceType: number) {
+    // You can check the values here:
+    // https://github.com/driftyco/ionic-native/blob/master/src/plugins/camera.ts
+    Camera.getPicture({
+      quality: 100,
+      destinationType: 0, // DATA_URL
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then(imageData => {
+      this.srcImage = `data:image/jpeg;base64,${imageData}`;
+    }, error => {
+        console.log(`ERROR -> ${JSON.stringify(error)}`);
+    });
+  }
+
+  analyze () {
+    let loading = Loading.create({
+      content: 'Reading...'
+    });
+    this.nav.present(loading);
+    OCRAD(document.getElementById('image'), text => {
+      loading.dismiss();
+      console.log(text);
+      alert(text);
+    });
+  }
+
+  restart () {
+    this.srcImage = '';
+    this.presentActionSheet();
   }
 
 }
